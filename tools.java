d4e76dd5a8c778303e3f93e1454b78c7315dfd23
leo <50470535+LeoLi0910@users.noreply.github.com>
2019-05-17 10:35:29 +0800

    Add files via upload

---
+++ tools.java
@@ -0,0 +1,79 @@
+public class tools{
+    /**print a line on the scree
+     * @param num how many times print charator str
+     * @param str the charator for printing 
+     */
+    public static void printLine(int num , String str) {
+        for(int i =0;i < num; i++){
+            System.out.print(str);
+    }
+        System.out.println();
+    }
+    public static void printSquare(int num , String str ) {
+        for(int i = 0; i<num;i++){
+         printLine(num,str);
+        }
+    }
+    public static void printRectangle(int width,int length,String str){
+        for (int r = 0;r<width;r++);{
+            printLine(length, str);
+        }
+    }
+    //get an integer from user input
+    public static int getInt(){
+        System.out.println("Enter an Integer;");
+        scanner in = new Scaaner(system.in) ;
+        int num = in.nextInt();
+        return num;
+    }
+    public static int getInt(string str){
+    System.out.println(str);
+    Scanner in = new Scaaner (system.in);
+    int num = in.nextInt();
+    return num;
+    }
+    //get a string
+    public static int getInt(string str){
+    System.out.println("Enter a String. ");
+    Scaaner in =new sacnner(system.in);
+    String s =in.nextLine();
+    return s;
+    }
+    public static int add(int a, int b){
+        return a + b;
+        //client program, tools.java
+        //main method
+    }
+        //**Change a to 5
+        public static void modify(int a){//local variable 局部变量
+            a = 5;
+    }
+        public static int change (int a){//return a new value 
+        return a - 5;
+    }
+        public static string weekday(int d){
+        if (d==1){
+            return "Monday";
+        }else if (d==2){
+            return "Tuesday";
+        }else if (d==3){
+            return "Wednesday";
+        }else if (d==4){
+            return "thuesday";
+        }else if (d==5){
+            return "Friday";
+        }else if (d==6){
+            return "Saturday";
+        }else if (d==7){
+            return "Sunday";
+        }
+        public String numtran(int n){
+            String tmp ="";
+            for (int i , , ,)
+            for (int j , , ,){
+                tmp+=
+            }
+            tmp+="";
+        }
+        return tmp
+    }
